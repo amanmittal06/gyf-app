503db7de7bf461c91e8193aa5e6bee3e77190c4b
amanmittal06 <amanagrawal061202@gmail.comm>
2024-09-17 05:11:26 +0530

    updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const server = express();
 const fs = require('fs')
 const mongoose = require('mongoose');
-const path = require('path')
 const { Schema } = mongoose;
 const productRouter = require('./routes/product')
 const userRouter = require('./routes/user')
@@ -23,12 +22,8 @@ async function main() {
 
 server.use(cors());
 server.use(express.json());
-server.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)))
 server.use('/products' , productRouter.router);
 server.use('/users' , userRouter.router);
-server.use('*', (req,res)=>{
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-})
 
 server.listen(process.env.PORT , ()=>{
     console.log('server started');
