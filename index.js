0ae2e8225982db213179ed1c418cbe1fe44819f4
amanmittal06 <amanagrawal061202@gmail.comm>
2024-09-28 03:16:24 +0530

    updated

--- index.js
+++ index.js
@@ -20,13 +20,7 @@ const allowedOrigins = [
 
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true); 
-    } else {
-      callback(new Error('Not allowed by CORS')); 
-    }
-  },
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true ,
   allowedHeaders: ['Content-Type', 'Authorization'],
