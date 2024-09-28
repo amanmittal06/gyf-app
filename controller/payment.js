cdb343eedc293196d342689f8f998f9b757fcaa9
amanmittal06 <amanagrawal061202@gmail.comm>
2024-09-28 06:27:16 +0530

    updated

--- controller/payment.js
+++ controller/payment.js
@@ -28,7 +28,32 @@ exports.checkout = async (req, res)=>{
 
 exports.paymentverification = async (req, res)=>{
     
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
+    .update(body.toString())
+    .digest("hex");
+
+  const isAuthentic = expectedSignature === razorpay_signature;
+
+  if (isAuthentic) {
+    // Database comes here
+
+    // await Payment.create({
+    //   razorpay_order_id,
+    //   razorpay_payment_id,
+    //   razorpay_signature,
+    // });
+
     res.redirect(
-        `http://localhost:5173/store`
+      `http://localhost:5173/store`
     );
+    } else {
+    res.status(400).json({
+      success: false,
+    });
+  }
 }
