e88264f1c4460dac001761132d241dab03cc6cf0
amanmittal06 <amanagrawal061202@gmail.comm>
2024-09-28 11:58:32 +0530

    updated

--- controller/storeuser.js
+++ controller/storeuser.js
@@ -5,8 +5,7 @@ exports.createStoreUser  = async (req,res) =>{
     try{
         const storeUser = await StoreUser.findOneAndUpdate({ email: req.body.email}, req.body , {upsert: true });
         console.log(`Store user saved successfully`);
-        res.send('Store user saved successfully').json(storeUser);
-        
+        res.json(storeUser);
     }
     catch{(err) => {
         res.status(400);
@@ -18,7 +17,7 @@ exports.getStoreUser = async (req,res) =>{
     emailId = req.params.email;
     try{
        const storeUser = await StoreUser.findOne({email: emailId});
-       res.send(storeUser);
+       res.json(storeUser);
     }
     catch(err){
         console.status(403).log(err);
