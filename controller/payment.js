62c65bedac77f99b07d78d85c475b9e1272d05c4
amanmittal06 <amanagrawal061202@gmail.comm>
2024-09-28 06:00:49 +0530

    updated

--- controller/payment.js
+++ controller/payment.js
@@ -4,20 +4,26 @@ const crypto = require('crypto');
 
 
 exports.checkout = async (req, res)=>{
-    const instance = razorpayInstance.getRazorpayInstance();
-    const amount = req.body.amount;
-    const options ={
+    try{
+        const instance = await razorpayInstance.getRazorpayInstance();
+        const amount = req.body.amount;
+        const options ={
         amount: amount*100,
         currency: "INR",
+        }
+        const order = await instance.orders.create(options);
+        res.status(200).json({
+            success: true,
+            order
+        })
+        console.log(order)
     }
-    const order = await instance.orders.create(options);
+    catch(err){
+        console.log
+    }
+    
 
-    res.status(200).json({
-        success: true,
-        order
-    })
-
-    console.log(order)
+    
 }
 
 exports.paymentverification = async (req, res)=>{
