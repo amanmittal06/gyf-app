4620060806b2b05f502160330a07124e4e0498e2
amanmittal06 <amanagrawal0139@gmail.com>
2025-04-26 06:19:50 +0530

    changed redirect location from orders to store

--- controller/payment.js
+++ controller/payment.js
@@ -48,7 +48,7 @@ exports.paymentverification = async (req, res)=>{
     const newOrder = new Order(currOrder);
     newOrder.save()
      .then(() => {
-        res.redirect('https://www.gyf.org.in/store');
+        res.redirect('https://www.gyf.org.in/orders');
       })
      .catch((err) => {
         res.status(400).json({
