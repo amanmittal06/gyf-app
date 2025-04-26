81b2be303ab27bcc5bfd4180642e893fe8323da2
amanmittal06 <amanagrawal0139@gmail.com>
2025-04-26 06:48:30 +0530

    .

--- controller/payment.js
+++ controller/payment.js
@@ -45,7 +45,7 @@ exports.paymentverification = async (req, res)=>{
   if (isAuthentic) {
     // Database comes here
 
-    currOrder = {...currOrder, paymentId:razorpay_payment_id}
+    currOrder = {...currOrder, paymentId:`${razorpay_payment_id}`}
 
     const newOrder = new Order(currOrder);
     newOrder.save()
